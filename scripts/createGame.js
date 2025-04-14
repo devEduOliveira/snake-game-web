cea5dcc4245d583ec35b28eb09f5d80678154da8
devEduOliveira <eduardooliveira.tecn@gmail.com>
2025-04-14 17:26:53 -0300

    feat: criando logica da comida

--- scripts/createGame.js
+++ scripts/createGame.js
@@ -1,7 +1,18 @@
 const gameBoard = document.querySelector(".gameBoard")
+const startPage = document.querySelector(".startPage") 
+const foodElement = document.querySelector(".food");
+const inputName = document.querySelector(".inputName")
+const ranking = document.querySelector(".scoreHigh")
+const score = document.querySelector(".score")
+const scoreLabel = document.querySelector(".scoreLabel")
+const rules = document.querySelector(".rules")
+const gameOver = document.querySelector(".gameOver")
 
+// global variables
 const gridSize = 20;
 let snake = [{x: 10, y:10}];
+let food = generateFood();
+let randomFood = generateFood()
 
 function createElement(tag, className){
     let element = document.createElement(tag);
@@ -12,6 +23,7 @@ function createElement(tag, className){
 function draw(){
     gameBoard.innerHTML = ``
     drawSnake()
+    drawFood()
 }
 
 function drawSnake(){
@@ -30,4 +42,35 @@ function drawSnake(){
 function setPosition(element, position){
     element.style.gridColumn = position.x;
     element.style.gridRow = position.y;
+}
+
+function drawRandomFood(){
+    if (snake.length > 4 && snake.length % 2 != 0) {
+        const randomFoodElement = createElement("div", "foodRandom")
+        setPosition(randomFoodElement, randomFood)
+        gameBoard.appendChild(randomFoodElement);
+    }
+}
+
+function drawFood(){
+    if(gameStarted){
+        const foodElement = createElement("div", "food")
+        setPosition(foodElement, food)
+        gameBoard.appendChild(foodElement); 
+        drawRandomFood()
+    }
+}
+
+function generateFood() {
+    let x, y;
+    let valid = false;
+
+    while (!valid) {
+        x = Math.floor(Math.random() * gridSize) + 1; 
+        y = Math.floor(Math.random() * gridSize) + 1;
+
+        valid = !snake.some(segment => segment.x === x && segment.y === y);
+    }
+
+    return { x, y };
 }
