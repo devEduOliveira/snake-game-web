051edddd459e8c83fc1f2f12c86d6297cfdaa142
devEduOliveira <eduardooliveira.tecn@gmail.com>
2025-04-14 16:48:55 -0300

    feat: adicionando score, input name, e botao para modal de regras

--- scripts/createPage.js
+++ scripts/createPage.js
@@ -8,10 +8,12 @@ function createStructure(){
 
     const startPage = createStartPage();
     const gameOverPage = createGameOverPage();
+    const scoreContainer = createScore();
 
     containerGame.appendChild(screenBorder)
     containerGame.appendChild(startPage)
     containerGame.appendChild(gameOverPage)
+    containerGame.appendChild(scoreContainer)
 }
 
 createStructure()
@@ -44,5 +46,55 @@ function createGameOverPage(){
     gameOverPage.appendChild(gameOverTitle)
     gameOverPage.appendChild(image)
     return gameOverPage;
+}
+
+function createScore(){
+    const scoreContainer = createElement("div", "scoreContainer")
+        const score = createElement("div", "score disable")
+        score.innerText = "000"
+
+        const scoreLabel = createElement("div", "scoreLabel disable")
+        scoreLabel.innerText = "SCORE"
+
+        const rules = createElement("div", "rules")
+        rules.setAttribute("onclick", "showModal()")
+        rules.innerText = "RULES"
+        
+        const scoreHigh = createElement("div", "scoreHigh")
+        scoreHigh.innerText = "RANKING"
+
+        const inputName = createElement("input", "inputName")
+        inputName.setAttribute("placeholder", "insert username")
+
+        const scoreRank = createElement("div", "scoreRank")
+            const scoreTable = createElement("table", "scoreRankTable") 
+            scoreTable.innerHTML = `
+                <tr>
+                    <th id="rank"></th>
+                    <th id="name">Name</th>
+                    <th id="score">Score</th>
+                </tr>
+            
+            `
+        scoreRank.appendChild(scoreTable)
+         
+    scoreContainer.appendChild(rules)        
+    scoreContainer.appendChild(score)
+    scoreContainer.appendChild(scoreLabel)
+    scoreContainer.appendChild(inputName)
+    scoreContainer.appendChild(scoreHigh)
+    scoreContainer.appendChild(scoreRank)
+    return scoreContainer;
+}
+
+function showScores(evenet){
+    const scoreRank = document.querySelector(".scoreRank")
+    if(evenet.type == "mouseover"){
+        scoreRank.style.display = 'block';
+        scoreRank.style.opacity = '1';
+    } else {
+        scoreRank.style.display = 'none';
+        scoreRank.style.opacity = '0';
+    } 
 }
 
